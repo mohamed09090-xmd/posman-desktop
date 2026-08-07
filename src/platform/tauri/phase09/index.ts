2147557c7419292ef9268e9fa0b461209a92340b
g.m0h1 <144628717+mohamed09090-xmd@users.noreply.github.com>
2026-08-07 05:46:58 +0100

    feat(phase09): export typed gateway surface

---
+++ src/platform/tauri/phase09/index.ts
@@ -0,0 +1,7 @@
+export * from "./contracts";
+export * from "./invokePhase09";
+export { templatesGateway } from "./templatesGateway";
+export { documentsGateway } from "./documentsGateway";
+export { reportsGateway } from "./reportsGateway";
+export { auditGateway } from "./auditGateway";
+export { backupGateway } from "./backupGateway";
